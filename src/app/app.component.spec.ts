3f9f3a519d25e1b8109fe899f37f618bcba8c0f6
Viswatej Parupudi <viswatej.parupudi@forestx.se>
2024-11-13 21:25:29 +0530

    Dynamic class style change

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,7 +17,6 @@ describe('AppComponent', () => {
   it(`should have the 'typing' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('typing');
   });
 
   it('should render title', () => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'typing';
+  randomText : string = lorem.sentence();
+  enteredText : string = '';
+  successMessage = "";
+
+  onInput(event: Event){
+    const target = event.target as HTMLInputElement;
+    this.enteredText = target.value;
+    console.log(target.value);
+  }
+
+  compare(randomLetter: string, enteredLetter: string){
+    if(!enteredLetter){
+      return 'pending';
+    }
+
+    return randomLetter === enteredLetter ? 'correct':'incorrect';
+  }
 }
